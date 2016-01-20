bf12e869b93da91c729e4b934b2e34aadde8da1c
Andrei Teodorescu <andrei@globalmouth.com>
2016-01-20 13:33:37 +0100

    Initial commit KSBWI-74

---
+++ src/js/controllers/appController.js
@@ -0,0 +1,42 @@
+(function () {
+
+   'use strict';
+
+   (function ( $app ) {
+      return $app.controller('appController',
+         ['$scope', '$controller', '$http', 'kambiAPIService', 'kambiWidgetService',
+            function ( $scope, $controller, $http, kambiAPIService, kambiWidgetService ) {
+
+               angular.extend(this, $controller('widgetCoreController', {
+                  '$scope': $scope
+               }));
+
+               $scope.defaultArgs = {
+               };
+
+               /**
+                * Keep the height to 0 until we get data
+                * @type {number}
+                */
+               $scope.defaultHeight = 450;
+
+               /**
+                * Listener for page:info, we get here the event id from pageParam
+                */
+               $scope.$on('PAGE:INFO', function ( e, data ) {
+               });
+
+               /**
+                * Request the page info in order to obtain the event id
+                */
+               kambiWidgetService.requestPageInfo();
+
+               /**
+                * Init the controller and get the poll data based on the event id
+                */
+               $scope.init().then(function () {
+               });
+
+            }]);
+   })(angular.module('facebookWidget'));
+})();
