a2fd763da699ee06f566c9958fd9776d38990d46
Konrad Szalwinski <konrad.szalwinski@globalmouth.com>
2016-09-07 13:36:26 +0200

    KSBWI-710 Setting up production configuration

---
+++ webpack.production.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const validate = require('webpack-validator');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = validate({
+   entry: {
+      'core.min': ['./src/index.js']
+   },
+   module: {
+      preLoaders: [
+         { test: /.js$/, exclude: /node_modules/, loader: 'jshint-loader' }
+      ],
+      loaders: [
+         {test: /\.svg/, loader: 'svg-url-loader'},
+         {test: /.js$/, exclude: /node_modules/, loader: 'babel-loader', query: {presets: ['es2015']}},
+         {
+            test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+            exclude: /node_modules/,
+            loader: 'url-loader?importLoaders=1&limit=100000'
+         },
+         {test: /\.(ttf|woff|eot|svg)$/, loader: 'file', query: {name: 'fonts/[name].[ext]'},},
+         {test: /\.json$/, loader: 'json'},
+         {test: /\.html/, loader: 'html-loader'},
+         {test: /\.scss$/, loaders: ['style', 'css', 'sass']}]
+   },
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
+      filename: '[name].js'
+   },
+   plugins: [
+      new webpack.optimize.CommonsChunkPlugin('common.js'),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+         compressor: {
+            warnings: true,
+         },
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new HtmlWebpackPlugin({ template: 'src/index.html'})
+   ],
+   resolve: {
+      extensions: ['', '.js', '.json', '.scss']
+   }
+});
