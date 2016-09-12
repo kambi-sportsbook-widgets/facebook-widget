1f57c94684de80a35c9409f908c0ec0ce5218f41
Konrad Szalwinski <konrad.szalwinski@globalmouth.com>
2016-09-12 16:02:00 +0200

    KSBWI-710 Removing unnecessary icons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import './index.html';
 import FacebookWidget from './js/app';
 import './scss/app.scss';
-import './fonts/kambi-icomoon.svg';
-import './fonts/kambi-icomoon.eot';
-import './fonts/kambi-icomoon.ttf';
-import './fonts/kambi-icomoon.woff';
 
 FacebookWidget();
